29be19fcaeac21118185ee069e01787c5582dfb1
TheJouster <tomilovdima999@gmail.com>
2024-10-24 00:13:57 +0500

    Add project files.

---
+++ Tyuiu.TomilovDA.Sprint3.Task0.V25.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.TomilovDA.Sprint3.Task0.V25.Lib;
+
+namespace Tyuiu.TomilovDA.Sprint3.Task0.V25.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService dataService = new DataService();
+            Assert.AreEqual(0.69, dataService.GetSumSeries(5,1,14));
+        }
+    }
+}
---
+++ Tyuiu.TomilovDA.Sprint3.Task0.V25/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.TomilovDA.Sprint3.Task0.V25.Lib;
+namespace Tyuiu.TomilovDA.Sprint3.Task0.V25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполнил: Томилов Дмитрий Алексеевич | ПКТб-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Оператор цикла for                                                      *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #25                                                             *");
+            Console.WriteLine("* Выполнил: Томилов Дмитрий Алексеевич | ПКТб-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Написать программу используя цикл for, которая вычисляет сумму ряда по фо*");
+            Console.WriteLine("*ормуле, при X=5                                                          *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите X");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите i");
+            int startValue = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите конечное число i");
+            int stopValue = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.GetSumSeries(x,startValue,stopValue);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
